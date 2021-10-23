8eb6276d5f68767f69a19155b9a59061ade9c108
mysoyul <vega2k_@naver.com>
2021-10-23 16:37:08 +0900

    flatmap

--- src/main/java/java8new/_02stream/basic2/_03Mapping.java
+++ src/main/java/java8new/_02stream/basic2/_03Mapping.java
@@ -1,17 +1,22 @@
 package java8new._02stream.basic2;
 
+import java8new._02stream.basic1.Dish;
+
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class _03Mapping {
 
     public static void main(String...args){
 
         //1. map -Dish의 name 목록만
-
-
+        List<String> nameList = Dish.menu.stream()
+                .map(Dish::getName)
+                .collect(toList());
+        nameList.forEach(System.out::println);
 
         // map 
         List<String> words = Arrays.asList("Hello", "World");
@@ -20,13 +25,23 @@ public class _03Mapping {
                                          .collect(toList());
         System.out.println(wordLengths);
 
-        //2. map - 중복된 문자 제거한 word 리스트
+        //2. map - 중복된 문자 제거하지 못한 word 리스트
+        words.stream()  //Stream<String>
+                .map(word -> word.split(""))   //Stream<String[]>
+                .distinct()
+                .forEach(System.out::println);
 
+        //3.flatMap  - 중복된 문자 제거한 word 리스트
+        words.stream()
+                .map(word -> word.split(""))
+                .flatMap(wordArr -> Arrays.stream(wordArr))
+                .distinct()
+                .forEach(System.out::println);
 
-        //3.flatMap  - 중복된 문자 제거가 word 리스트
-
-
-
+        words.stream()
+                .flatMap(word -> Arrays.stream(word.split("")))
+                .distinct()
+                .forEach(System.out::print);
 
         // flatMap
         List<Integer> numbers1 = Arrays.asList(1,2,3,4,5);
