c0529360c681b2dbc2f835414f32f359aedab59f
mysoyul <vega2k_@naver.com>
2021-10-30 11:32:54 +0900

    Collectors method

--- src/main/java/java8new/_02stream/collect/_02Summarizing.java
+++ src/main/java/java8new/_02stream/collect/_02Summarizing.java
@@ -1,7 +1,9 @@
 package java8new._02stream.collect;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
 import static java8new._02stream.collect.Dish.menu;
 
@@ -10,7 +12,7 @@ public class _02Summarizing {
     public static void main(String ... args) {
         System.out.println("Nr. of dishes: " + howManyDishes());
         System.out.println("The most caloric dish is: " + findMostCaloricDish());
-        System.out.println("The most caloric dish is: " + findMostCaloricDishUsingComparator());
+        System.out.println("The least caloric dish is: " + findMostCaloricDishUsingComparator());
         System.out.println("Total calories in menu: " + calculateTotalCalories());
         System.out.println("Average calories in menu: " + calculateAverageCalories());
         System.out.println("Menu statistics: " + calculateMenuStatistics());
@@ -23,38 +25,40 @@ public class _02Summarizing {
         return menu.stream().collect(counting());
     }
 
-    //1. Comparator를 사용한 collect(), reducing()
+    //1. Comparator를 사용한 collect(), Collectors.mixBy()를 사용해서 최소 칼로리
     private static Dish findMostCaloricDishUsingComparator() {
 
-        return null;
+        return menu.stream().collect(minBy(comparingInt(Dish::getCalories))).get();
     }
 
-    //collect() - reducing 사용
+    //1-1. collect() - Collectors.reducing() 사용해서 최대 칼로리
     private static Dish findMostCaloricDish() {
         return menu.stream().collect(reducing((d1, d2) -> d1.getCalories() > d2.getCalories() ? d1 : d2)).get();
     }
 
-    //2.  summingInt() 사용
+    //2.  summingInt() 사용해서 칼로리 합계
     private static int calculateTotalCalories() {
 
-        return 0;
+        return menu.stream().collect(summingInt(Dish::getCalories));
     }
 
-    //3. averagingInt() 사용
+    //3. averagingInt() 사용해서 칼로리 평균
     private static Double calculateAverageCalories() {
 
-        return 0.0;
+        return menu.stream().collect(averagingInt(Dish::getCalories));
     }
 
-    //4. summarizingInt() 사용
+    //4. summarizingInt() 사용해서 집계함수
     private static IntSummaryStatistics calculateMenuStatistics() {
-        return null;
+
+        return menu.stream().collect(summarizingInt(Dish::getCalories));
     }
 
     //5. joining() 사용
     private static String getShortMenu() {
-
-        return "";
+        return menu.stream() //Stream<Dish>
+                .map(dish -> dish.getName()) //Stream<String>
+                .collect(Collectors.joining(" "));
     }
 
     private static String getShortMenuCommaSeparated() {
